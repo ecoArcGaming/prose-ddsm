85c5981ede4e597b218d65adb28ef4a39bffb38e
ecoArcGaming <erik304501@gmail.com>
2025-05-25 16:10:48 -0400

    added time activation

---
+++ train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:l40s:1
+#SBATCH -p gpu_quad
+#SBATCH -t 2-12:00
+#SBATCH --mem=50GB
+#SBATCH --output=/n/groups/marks/users/erik/prose-ddsm/slurm/ddsm%j.out
+#SBATCH --error=/n/groups/marks/users/erik/prose-ddsm/slurm/slurm/ddsm%j.err
+#SBATCH --job-name="ddsm"
+
+source activate base
+conda activate ddsm
+export LD_LIBRARY_PATH=/home/jix836/.conda/envs/promoet/lib:${LD_LIBRARY_PATH}
+wandb login 6d15eff7893c02d9755493383bac182122241aae
+cd /n/groups/marks/users/erik/prose-ddsm
+python scripts/train.py
